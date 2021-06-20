b7ed44a8352b95ea90ff8a3564ce313f21f64e34
cwj980119 <cwj980119@naver.com>
2021-06-21 00:29:40 +0900

    fix_player_animation
    상하좌우 이동 애니매이션수정
    상하좌우 코드 합침(수정 필요)

--- Escape_T/Assets/PlayerController.cs
+++ Escape_T/Assets/PlayerController.cs
@@ -22,23 +22,23 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetAxisRaw("Horizontal") > 0 || Input.GetAxisRaw("Horizontal") < 0)
+        if (Input.GetAxisRaw("Horizontal") > 0 || Input.GetAxisRaw("Horizontal") < 0 || Input.GetAxisRaw("Vertical") > 0 || Input.GetAxisRaw("Vertical") < 0)
         {
-            transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * move_speed * Time.deltaTime, 0f, 0f));
+            transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * move_speed * Time.deltaTime, Input.GetAxisRaw("Vertical") * move_speed * Time.deltaTime, 0f));
             animator.SetInteger(animationState, (int)States.move);
         }
 
-        if (Input.GetAxisRaw("Vertical") > 0 || Input.GetAxisRaw("Vertical") < 0)
+        /*if (Input.GetAxisRaw("Vertical") > 0 || Input.GetAxisRaw("Vertical") < 0)
         {
             transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * move_speed * Time.deltaTime, 0f));
             animator.SetInteger(animationState, (int)States.move2);
-        }
+        }*/
         else
         {
             animator.SetInteger(animationState, (int)States.stop);
 
             // 플레이어 속도에 따라서 애니메이션 속도 변경
-            this.animator.speed = move_speed / 2.0f;
+            this.animator.speed = move_speed / 1.0f;
         }
     }
 }
