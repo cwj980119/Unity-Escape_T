cc1428dbf74b12e473e81d90dbad44763cd52a72
baikdk <baikdk@g.hongik.ac.kr>
2021-06-20 23:38:10 +0900

    애니메이션 정지, 움직임 구현
    좌우로 움직이지는 않음

--- Escape_T/Assets/PlayerController.cs
+++ Escape_T/Assets/PlayerController.cs
@@ -6,6 +6,13 @@ public class PlayerController : MonoBehaviour
 {
     public float move_speed; //캐릭터 이동속도
     Animator animator;
+    string animationState = "AnimationState";
+    enum States
+    {
+        stop = 2,
+        move = 1,
+        move2 = 3
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -15,17 +22,23 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetAxisRaw("Horizontal")>0 || Input.GetAxisRaw("Horizontal") < 0)
+        if (Input.GetAxisRaw("Horizontal") > 0 || Input.GetAxisRaw("Horizontal") < 0)
         {
             transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * move_speed * Time.deltaTime, 0f, 0f));
+            animator.SetInteger(animationState, (int)States.move);
         }
 
         if (Input.GetAxisRaw("Vertical") > 0 || Input.GetAxisRaw("Vertical") < 0)
         {
             transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * move_speed * Time.deltaTime, 0f));
+            animator.SetInteger(animationState, (int)States.move2);
         }
+        else
+        {
+            animator.SetInteger(animationState, (int)States.stop);
 
-        // 플레이어 속도에 따라서 애니메이션 속도 변경
-        this.animator.speed = move_speed / 2.0f;
+            // 플레이어 속도에 따라서 애니메이션 속도 변경
+            this.animator.speed = move_speed / 2.0f;
+        }
     }
 }
