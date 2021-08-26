fdf75cb4312d08ef2f2812c7cf92098e86a6c6f3
yellowon0103 <86039114+yellowon0103@users.noreply.github.com>
2021-08-26 21:42:50 +0900

    도어락 문열림 구현
    문열림 이미지 추가 + 구현

--- Escape_T/Assets/김_도어락/CodePanel.cs
+++ Escape_T/Assets/김_도어락/CodePanel.cs
@@ -9,13 +9,24 @@ public class CodePanel : MonoBehaviour {
 	Text codeText;
 	string codeTextValue = "";
 
+	// 테스트
+	public Image Openlock;
+
+	void Start()
+    {
+		Openlock.enabled = false;
+    }
+
+	//테스트
+
 	// Update is called once per frame
 	void Update () {
 		codeText.text = codeTextValue;
 
 		if (codeTextValue == "1234") {
 			//student.isSafeOpened = true;
-			codeTextValue = "9999";
+			// 그냥 내가 해본거 codeTextValue = "9999";
+			Openlock.enabled = true;
 		}
 
 		if (codeTextValue.Length >= 4)
