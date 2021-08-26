1e02aefe7c539a98e1d246d5068e6436a37f7a1a
yellowon0103 <86039114+yellowon0103@users.noreply.github.com>
2021-08-26 19:37:29 +0900

    도어락 숫자 입력가능
    대신 문제 solve는 구현 안됨

---
+++ Escape_T/Assets/김_도어락/CodePanel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CodePanel : MonoBehaviour {
+
+	[SerializeField]
+	Text codeText;
+	string codeTextValue = "";
+
+	// Update is called once per frame
+	void Update () {
+		codeText.text = codeTextValue;
+
+		if (codeTextValue == "1234") {
+			//student.isSafeOpened = true;
+			codeTextValue = "9999";
+		}
+
+		if (codeTextValue.Length >= 4)
+			codeTextValue = "";
+	}
+
+	public void AddDigit(string digit)
+	{
+		codeTextValue += digit;
+	}
+
+}
