3e2667d0a548b0bb263ce49083780a22347bd92c
cwj980119 <cwj980119@naver.com>
2021-08-27 10:32:26 +0900

    fix_bbgame

--- Escape_T/Assets/NumberGameController.cs
+++ Escape_T/Assets/NumberGameController.cs
@@ -1,6 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragExample : MonoBehaviour,IDragHandler,IBeginDragHandler, IEndDragHandler
+{
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("Start");
+        //throw new System.NotImplementedException();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("Draging");
+        //throw new System.NotImplementedException();
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("EndDrag");
+        // throw new System.NotImplementedException();
+    }
+}
 
 public class NumberGameController : MonoBehaviour
 {
