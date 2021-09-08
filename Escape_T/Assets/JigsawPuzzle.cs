72dbb68629f7e3e44219c1246584c8220593565b
baikdk <baikdk@g.hongik.ac.kr>
2021-09-08 13:50:07 +0900

    퍼즐1
    puzzle one

---
+++ Escape_T/Assets/JigsawPuzzle.cs
@@ -0,0 +1,21 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JigsawPuzzle : MonoBehaviour
+{
+    public GameObject PuzzlePosSet;
+    public GameObject PuzzlePieceSet;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
