3e8732c26453d8c5018e1a2ebdd421b22afd7e70
yellowon0103 <86039114+yellowon0103@users.noreply.github.com>
2021-09-12 14:42:10 +0900

    대화 연속 구현

--- Escape_T/Assets/대화창폰트/TalkManager.cs
+++ Escape_T/Assets/대화창폰트/TalkManager.cs
@@ -8,21 +8,43 @@ public class TalkManager : MonoBehaviour
     public Text tx;
     private string m_text = "박현서 늦잠 잤대여 에베베베";
 
+    List<string> talkText = new List<string>();
+    int i = 0;
+
     // Start is called before the first frame update
     void Start()
     {
+        talkText.Add("안녕하세요");
+        talkText.Add("반가워요");
+        talkText.Add("다시 만나요~");
+        talkText.Add("야 안녕~\n내일 석식 뭐냐 궁금해!!");
+
+
         StartCoroutine(_typing());
     }
 
     IEnumerator _typing()
     {
-        yield return new WaitForSeconds(2f);
+        //yield return new WaitForSeconds(2f);
 
         for (int i = 0; i <= m_text.Length; i++) 
         {
             tx.text = m_text.Substring(0, i);
 
-            yield return new WaitForSeconds(0.15f);
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+
+    void Update()
+    {
+        if (i < talkText.Count)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                m_text = talkText[i];
+                i++;
+                StartCoroutine(_typing());
+            }
         }
     }
 }
