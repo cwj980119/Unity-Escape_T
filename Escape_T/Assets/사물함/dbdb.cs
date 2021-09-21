3f74de0959296cfa1690d59c0244fc68376963f9
yellowon0103 <86039114+yellowon0103@users.noreply.github.com>
2021-09-22 05:38:27 +0900

    2진수10진수 코드

---
+++ Escape_T/Assets/사물함/dbdb.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dbdb : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    /*   int tenConvertTwo(int n)
+    {
+        int result = 0;
+        for(int i=0;n>0;i*=10)
+        {
+            int binary = n % 2;
+            result += binary * i;
+            n /= 2;
+        }
+        return result;
+    }
+
+    int twoConvertTen(int n)
+    {
+        int result = 0;
+        int mul = 1;
+        while(num>0)
+        {
+            if(n%2)
+            { 
+                result += mul; 
+            }
+            mul *= 2;
+            num /= 10;
+        }
+        return result;
+    }
+    */
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
