52cb4c042647dd1a227d3031ce7ac33201ac77e9
baikdk <baikdk@g.hongik.ac.kr>
2021-09-22 00:32:07 +0900

    55
    55

---
+++ Escape_T/Assets/6층/DoorLock1.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DoorLock1 : MonoBehaviour
+{
+
+	[SerializeField]
+	Text codeText;
+	string codeTextValue = "";
+
+	// 테스트
+	public Image Openlock;
+
+	void Start()
+	{
+
+	}
+
+	//테스트
+
+	// Update is called once per frame
+	void Update()
+	{
+		codeText.text = codeTextValue;
+
+		if (codeTextValue == "21")
+		{
+			//student.isSafeOpened = true;
+			// 그냥 내가 해본거 codeTextValue = "9999";
+			GameObject.Find("Canvas").transform.GetChild(14).gameObject.SetActive(false);
+			GameObject.Find("Canvas-world").transform.GetChild(7).gameObject.SetActive(false);
+		}
+
+		if (codeTextValue.Length >= 2)
+			codeTextValue = "";
+	}
+
+	public void AddDigit(string digit)
+	{
+		codeTextValue += digit;
+	}
+
+}
