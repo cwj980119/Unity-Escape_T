c2db7b5cb2bf1bf7fd7727949b8d4a694ab1af39
baikdk <baikdk@g.hongik.ac.kr>
2021-09-22 22:38:31 +0900

    Revert "인벤토리(실행만 됨)"
    This reverts commit d00c68be8532e6b6cb3eaa58422215c8e3c8c021.

--- Escape_T/Assets/Item.cs
+++ Escape_T/Assets/Item.cs
@@ -2,32 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[System.Serializable]
-public class Item
+[CreateAssetMenu]
+public class Item : ScriptableObject
 {
-    public int itemID; //아이템 일련번호
-    public string itemName; // 아이템 이름
-    public string itemDescription; //아이템 설명
-    public int itemCount; //아이템 개수
-    public Sprite itemIcon; //아이템 모양(그림)
-    public ItemType itemType; //아이템 종류
-
-    public enum ItemType
-    {
-        Use, // 소모품
-        Equip, // 장비
-        Install, // 설치
-        ETC //기타
-    }
-
-    public Item(int _itemID, string _itemName, string _itemDes, ItemType _itemType, int _itemCount=1)
-    {
-        itemID = _itemID;
-        itemName = _itemName;
-        itemDescription = _itemDes;
-        itemType = _itemType;
-        itemCount = _itemCount;
-        itemIcon = Resources.Load("ItemIcon/" + _itemID.ToString(), typeof(Sprite)) as Sprite;
-
-    }
+    public string itemName;
+    public Sprite itemImage;
 }
