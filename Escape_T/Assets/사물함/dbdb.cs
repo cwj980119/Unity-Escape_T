9fcc77f5590bae926e9c4c00eb267b4b8bd4a6dd
yellowon0103 <86039114+yellowon0103@users.noreply.github.com>
2021-09-22 20:50:08 +0900

    사물함-학번입력구현남음

--- Escape_T/Assets/사물함/dbdb.cs
+++ Escape_T/Assets/사물함/dbdb.cs
@@ -1,48 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class dbdb : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    /*   int tenConvertTwo(int n)
-    {
-        int result = 0;
-        for(int i=0;n>0;i*=10)
-        {
-            int binary = n % 2;
-            result += binary * i;
-            n /= 2;
-        }
-        return result;
-    }
+	[SerializeField]
+	Text codeText;
+	string codeTextValue = "";
 
-    int twoConvertTen(int n)
-    {
-        int result = 0;
-        int mul = 1;
-        while(num>0)
-        {
-            if(n%2)
-            { 
-                result += mul; 
-            }
-            mul *= 2;
-            num /= 10;
-        }
-        return result;
-    }
-    */
+	// 테스트
+	public Image Openlock;
 
+	void Start()
+	{
+		Openlock.enabled = true;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	}
+
+	//테스트
+
+	// Update is called once per frame
+	void Update()
+	{
+		codeText.text = codeTextValue;
+
+		if (codeTextValue == "1234")
+		{
+			//student.isSafeOpened = true;
+			// 그냥 내가 해본거 codeTextValue = "9999";
+
+			Openlock.enabled = false;
+
+		}
+
+		if (codeTextValue.Length >= 4)
+			codeTextValue = "";
+	}
+
+	public void AddDigit(string digit)
+	{
+		codeTextValue += digit;
+	}
+
 }
--- Escape_T/Assets/사물함/hello.cs
+++ Escape_T/Assets/사물함/hello.cs
@@ -13,12 +13,24 @@ public class hello : MonoBehaviour
 	// 테스트
 	public Image Openlock;
 
+	// 수정
+	[SerializeField]
+	Text Htext;
+	string HtextValue = "";
+	//
+
 	void Start()
 	{
-		Openlock.enabled = false;
+		Openlock.enabled = true;
+
 	}
 
-	//테스트
+	//수정
+	void Check_Htext()
+    {
+
+    }
+	//
 
 	// Update is called once per frame
 	void Update()
@@ -29,7 +41,9 @@ public class hello : MonoBehaviour
 		{
 			//student.isSafeOpened = true;
 			// 그냥 내가 해본거 codeTextValue = "9999";
-			Openlock.enabled = true;
+
+			Openlock.enabled = false;
+
 		}
 
 		if (codeTextValue.Length >= 4)
@@ -42,3 +56,33 @@ public class hello : MonoBehaviour
 	}
 
 }
+
+
+/*   int tenConvertTwo(int n)
+{
+    int result = 0;
+    for(int i=0;n>0;i*=10)
+    {
+        int binary = n % 2;
+        result += binary * i;
+        n /= 2;
+    }
+    return result;
+}
+
+int twoConvertTen(int n)
+{
+    int result = 0;
+    int mul = 1;
+    while(num>0)
+    {
+        if(n%2)
+        { 
+            result += mul; 
+        }
+        mul *= 2;
+        num /= 10;
+    }
+    return result;
+}
+*/
