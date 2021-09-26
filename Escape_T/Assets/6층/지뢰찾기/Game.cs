229fcc8e853a25569baf9f9ca9add8309167b237
baikdk <baikdk@g.hongik.ac.kr>
2021-09-26 12:57:56 +0900

    minesweeper2
    2nd

--- Escape_T/Assets/6층/지뢰찾기/Game.cs
+++ Escape_T/Assets/6층/지뢰찾기/Game.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Game : MonoBehaviour
 {
-    public GameObject[,] grid = new GameObject[9, 9];
+    public Tile[,] grid = new Tile[9, 9];
     // Start is called before the first frame update
     void Start()
     {
@@ -12,13 +12,31 @@ public class Game : MonoBehaviour
         {
             PlaceMines();
         }
-        
+
+        PlaceClues();
+
+        PlaceBlanks();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CheckInput();
+    }
+
+    void CheckInput()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            int x = Mathf.RoundToInt(mousePosition.x);
+            int y = Mathf.RoundToInt(mousePosition.y);
+
+            Tile tile = grid[x, y];
+
+            tile.SetIsCovered(false);
+        }
     }
 
     void PlaceMines()
@@ -28,7 +46,7 @@ public class Game : MonoBehaviour
 
         if (grid[x, y] == null)
         {
-            GameObject mineTile = Instantiate(Resources.Load("Prefab/Mine", typeof(GameObject)), new Vector3(x, y, 0), Quaternion.identity) as GameObject;
+            Tile mineTile = Instantiate(Resources.Load("Prefab/Mine", typeof(Tile)), new Vector3(x, y, 0), Quaternion.identity) as Tile;
             grid[x, y] = mineTile;
             Debug.Log("(" + x + "," + y + ")");
 ;        }
@@ -36,6 +54,115 @@ public class Game : MonoBehaviour
         else
         {
             PlaceMines();
+        }
+    }
+
+    void PlaceClues()
+    {
+        for(int y=0; y<9;y++)
+        {
+            for(int x=0;x<9;x++)
+            {
+                if(grid[x,y]==null)
+                {
+                    int numMines = 0;
+                    
+                    //ºÏ
+                    if (y + 1 < 9)
+                    {
+                        if (grid[x, y + 1] != null && grid[x, y+1].tileKind == Tile.TileKind.Mine)
+                        {
+                            numMines++;
+                        }
+                    }
+
+                    //µ¿
+                    if(x+1<9)
+                    {
+                        if(grid[x+1,y]!=null && grid[x+1, y].tileKind == Tile.TileKind.Mine)
+                        {
+                            numMines++;
+                        }
+                    }
+
+                    //³²
+                    if(y-1>=0)
+                    {
+                        if(grid[x,y-1]!=null && grid[x, y - 1].tileKind == Tile.TileKind.Mine)
+                        {
+                            numMines++;
+                        }
+                    }
+
+                    //µ¿
+                    if(x-1>=0)
+                    {
+                        if(grid[x-1,y]!=null && grid[x-1, y].tileKind == Tile.TileKind.Mine)
+                        {
+                            numMines++;
+                        }
+                    }
+
+                    //ºÏµ¿
+                    if(x+1<9 && y+1<9)
+                    {
+                        if(grid[x+1,y+1]!=null && grid[x+1, y + 1].tileKind == Tile.TileKind.Mine)
+                        {
+                            numMines++;
+                        }
+                    }
+
+                    //ºÏ¼­
+                    if(x-1>=0 && y+1<9)
+                    {
+                        if(grid[x-1,y+1]!=null && grid[x-1, y + 1].tileKind == Tile.TileKind.Mine)
+                        {
+                            numMines++;
+                        }
+                    }
+
+                    //³²µ¿
+                    if(x+1<9 && y-1>=0)
+                    {
+                        if(grid[x+1,y-1]!=null && grid[x+1, y - 1].tileKind == Tile.TileKind.Mine)
+                        {
+                            numMines++;
+                        }
+                    }
+
+                    //³²¼­
+                    if(x-1>=0 && y-1>=0)
+                    {
+                        if(grid[x-1,y-1]!=null && grid[x-1, y - 1].tileKind == Tile.TileKind.Mine)
+                        {
+                            numMines++;
+                        }
+                    }
+
+                    if(numMines>0)
+                    {
+                        Tile clueTile = Instantiate(Resources.Load("Prefab/" + numMines, typeof(Tile)), new Vector3(x, y, 0), Quaternion.identity) as Tile;
+
+                        grid[x, y] = clueTile;
+                    }
+                }
+            }
+        }
+    }
+
+    void PlaceBlanks()
+    {
+        for(int y=0;y<9;y++)
+        {
+            for(int x=0;x<9;x++)
+            {
+                if(grid[x,y]==null)
+                {
+                    Tile blankTile = Instantiate(Resources.Load("Prefab/blank", typeof(Tile)), new Vector3(x, y, 0), Quaternion.identity) as Tile;
+
+                    grid[x, y] = blankTile;
+                }
+            }
         }
     }
 }
