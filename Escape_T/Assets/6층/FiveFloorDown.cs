31407904fc97f6d9fdf77fe9158e1ca1031b77b1
baikdk <baikdk@g.hongik.ac.kr>
2021-09-29 02:30:29 +0900

    층간 이동
    move

---
+++ Escape_T/Assets/6층/FiveFloorDown.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FiveFloorDown : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "student")
+        {
+            SceneManager.LoadScene(2);
+        }
+    }
+}
