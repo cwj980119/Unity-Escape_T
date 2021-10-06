c11302dee169365deda670cc8f58ea5cd58e27ad
cwj980119 <cwj980119@naver.com>
2021-10-06 21:39:32 +0900

    add_DoorLockToggle

--- Escape_T/Assets/7층/DoorLockToggle.cs
+++ Escape_T/Assets/7층/DoorLockToggle.cs
@@ -6,4 +6,29 @@ using UnityEngine.SceneManagement;
 
 public class DoorLockToggle : MonoBehaviour
 {
+    [SerializeField]
+    GameObject Door, Canvas;
+    string codeTextValue = "";
+
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (codeTextValue == "1234")
+		{
+			//student.isSafeOpened = true;
+			// �׳� ���� �غ��� codeTextValue = "9999";
+			Door.SetActive(false);
+            Canvas.SetActive(false);
+		}
+
+		if (codeTextValue.Length >= 4)
+			codeTextValue = "";
+	}
+
+	public void AddDigit(string digit)
+	{
+		codeTextValue += digit;
+	}
+
 }
