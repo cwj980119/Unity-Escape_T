9815a83e579cf9a78f6aedb649a04f2c50f487ae
jungyungee <jungyungee0610@gmail.com>
2021-10-09 21:38:25 +0900

    우선 저장..

---
+++ Escape_T/Assets/7층/ComputerQuiz.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ComputerQuiz : MonoBehaviour
+{
+
+	[SerializeField]
+	Text codeText;
+	Text codeText2;
+	string codeTextValue = "";
+
+	// 테스트
+	public Image Openlock;
+
+	void Start()
+	{
+		Openlock.enabled = false;
+	}
+
+	//테스트
+
+	// Update is called once per frame
+	void Update()
+	{
+		codeText.text = codeTextValue;
+
+		if (codeTextValue == "46")
+		{
+			Openlock.enabled = true;
+		}
+
+		if (codeTextValue.Length >= 2)
+			codeTextValue = "";
+
+	}
+
+
+	public void AddDigit(string digit)
+	{
+		codeTextValue += digit;
+	}
+
+}
