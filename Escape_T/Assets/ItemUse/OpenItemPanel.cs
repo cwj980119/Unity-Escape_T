6dbe653f193fd31a8de246df5b9520d65a51745d
cwj980119 <cwj980119@g.hongik.ac.kr>
2021-10-10 20:11:21 +0900

    fix_getUI_Item

--- Escape_T/Assets/ItemUse/OpenItemPanel.cs
+++ Escape_T/Assets/ItemUse/OpenItemPanel.cs
@@ -6,6 +6,27 @@ public class OpenItemPanel : MonoBehaviour
 {
     [SerializeField]
     GameObject panel;
+    public Collider2D player;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+       if(collision.CompareTag("Player"))
+        {
+            player = collision;
+            
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision){
+        player = null;
+    }
+
+    public void GetUiItem(){
+        if(player!=null){
+            player.gameObject.GetComponent<Inven>().GetUiItem();
+        }
+    }
 
     public void OpenPanel(){
         panel.SetActive(true);
