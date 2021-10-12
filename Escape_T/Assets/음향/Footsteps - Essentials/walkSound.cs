6d94addbddf9b2422255edd1ce9d9d3c4cd779ce
yellowon0103 <86039114+yellowon0103@users.noreply.github.com>
2021-10-12 21:05:27 +0900

    rlekf

---
+++ Escape_T/Assets/음향/Footsteps - Essentials/walkSound.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class walkSound : MonoBehaviour
+{
+    [SerializeField] float speed = 5f;
+    float moveX:
+    Rigidbody2D rb:
+    AudioSource audioSrc; 
+    bool isMoving = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>(); audioSrc Get Component<AudioSource>():
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
