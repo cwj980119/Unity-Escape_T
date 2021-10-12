490f34da0ffc6440825f8d95d0a05d5ec021d02c
yellowon0103 <86039114+yellowon0103@users.noreply.github.com>
2021-10-12 21:44:55 +0900

    수정

--- Escape_T/Assets/음향/Footsteps - Essentials/walkSound.cs
+++ Escape_T/Assets/음향/Footsteps - Essentials/walkSound.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class walkSound : MonoBehaviour
 {
+    /*
     [SerializeField] float speed = 5f;
     float moveX:
     Rigidbody2D rb:
@@ -13,12 +14,35 @@ public class walkSound : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>(); audioSrc Get Component<AudioSource>():
+        rb = GetComponent<Rigidbody2D>(); 
+        audioSrc = GetComponent<AudioSource>():
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MoveSfx();
     }
+
+    void MoveSfx()
+    {
+        moveX = Input.GetAxis("Horizontal") * speed; 
+        rb.velocity = new Vector2(noveX, rb.velocity.y);
+
+        if (rb.velocity.x != 0) 
+            isMoving = true;
+
+        else 
+            isMoving = false;
+
+        if (isMoving)
+        {
+            if(!audioSrc.isPlaying) 
+                audioSrc.Play();
+        }
+
+        else 
+            audioSrc.Stop();
+    }
+    */
 }
