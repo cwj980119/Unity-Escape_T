c8e2d63e118c64690b76a30af21751bbbc21a1dc
baikdk <baikdk@g.hongik.ac.kr>
2021-10-13 22:04:13 +0900

    audio1
    audio

--- Escape_T/Assets/Scrpits/AudioController.cs
+++ Escape_T/Assets/Scrpits/AudioController.cs
@@ -6,9 +6,35 @@ using UnityEngine.SceneManagement;
 public class AudioController : MonoBehaviour
 {
     public static AudioController Musik;
-    public void TurnMusic()
+
+    public AudioSource BackGround;
+   // public AudioSource Effect[] = new AudioSource;
+    public List<AudioSource> Effect = new List<AudioSource>();
+
+    public void TurnOffMusic()
     {
-        AudioListener.volume = AudioListener.volume == 0?1:0;
+        BackGround.Pause();
+    }
+
+    public void TurnOnMusic()
+    {
+        BackGround.Play();
+    }
+
+    public void TurnEffectOnMusic()
+    {
+        for (int i=0; i<Effect.Count; i++)
+        {
+            Effect[i].Play();
+        }
+    }
+
+    public void TurnEffectOffMusic()
+    {
+        for (int i = 0; i < Effect.Count; i++)
+        {
+            Effect[i].Pause();
+        }
     }
     // Start is called before the first frame update
     void Start()
