764ba331392f383d5e6d4804e165fa14f23d61d3
yellowon0103 <86039114+yellowon0103@users.noreply.github.com>
2021-10-29 06:56:33 +0900

    수정

--- Escape_T/Assets/Scrpits/Systems/MovePosition.cs
+++ Escape_T/Assets/Scrpits/Systems/MovePosition.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class MovePosition : MonoBehaviour
 {
     public GameObject move_point;
-    
+    //
+    public GameObject panel;
+    //
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
        if(collision.CompareTag("Player"))
         {
+            //
+            panel.gameObject.SetActive(true);
+            //
             collision.transform.position = move_point.transform.position;
         }
 
