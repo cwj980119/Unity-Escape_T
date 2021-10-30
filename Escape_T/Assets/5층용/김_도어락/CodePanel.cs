feb173d3ccb9f67e9b71673a255f5459f03b561a
yellowon0103 <86039114+yellowon0103@users.noreply.github.com>
2021-10-30 18:39:45 +0900

    코드수정

--- Escape_T/Assets/5층용/김_도어락/CodePanel.cs
+++ Escape_T/Assets/5층용/김_도어락/CodePanel.cs
@@ -9,27 +9,20 @@ public class CodePanel : MonoBehaviour {
 	Text codeText;
 	string codeTextValue = "";
 
-	// 테스트
 	public Image Openlock;
 	public GameObject panel;
-
 
 	void Start()
     {
 		Openlock.enabled = false;
-		//GameObject.Find("Canvas_도어락").transform.GetChild(1).gameObject.SetActive(false);
 		panel.gameObject.SetActive(false);
 	}
-
-	//테스트
 
 	// Update is called once per frame
 	void Update () {
 		codeText.text = codeTextValue;
 
 		if (codeTextValue == "32112") {
-			//student.isSafeOpened = true;
-			// 그냥 내가 해본거 codeTextValue = "9999";
 			Openlock.enabled = true;
 			GameObject.Find("Canvas_도어락").transform.GetChild(1).gameObject.SetActive(true);
 			panel.gameObject.SetActive(true);
@@ -43,5 +36,4 @@ public class CodePanel : MonoBehaviour {
 	{
 		codeTextValue += digit;
 	}
-
 }
